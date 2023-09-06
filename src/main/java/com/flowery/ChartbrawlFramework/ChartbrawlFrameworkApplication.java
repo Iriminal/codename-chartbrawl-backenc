f360f279f2e01ab13acc5e7158f28aa89f2e3e32
v3lmelt <1131288411@qq.com>
2023-09-06 11:37:17 +0800

    Created spring-boot project.

---
+++ src/main/java/com/flowery/ChartbrawlFramework/ChartbrawlFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.flowery.ChartbrawlFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartbrawlFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartbrawlFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowery/ChartbrawlFramework/ChartbrawlFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowery.ChartbrawlFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartbrawlFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
